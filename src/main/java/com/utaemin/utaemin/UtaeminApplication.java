921eeb130e24009527684014a5c972a3e82b6c55
Changmin Kim <ksjchm4@gmail.com>
2024-12-02 14:41:02 +0900

    set. 초기 세팅

---
+++ src/main/java/com/utaemin/utaemin/UtaeminApplication.java
@@ -0,0 +1,13 @@
+package com.utaemin.utaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtaeminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtaeminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utaemin/utaemin/UtaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utaemin.utaemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
